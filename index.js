538979928498dda51097aafb281a638a0cc5d0c7
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-13 00:08:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,10 +39,11 @@ function removeFromCart(item) {
  
 function placeOrder(cardNumber) {
 
-  if (typeof(cardNumber) === "undefined"){
-    return "Sorry, we don't have a credit card on file for you."
-  } else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.")
   }
-  cart.length = 0;
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+
+  cart = []
 }
